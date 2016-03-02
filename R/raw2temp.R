1a5454f8708c2d814b1001ed9ac0cc6d2802946a
Glenn J Tattersall <gtatters@brocku.ca>
2016-03-01 19:35:39 -0500

    fixed comments

--- R/raw2temp.R
+++ R/raw2temp.R
@@ -9,16 +9,20 @@ function(raw,E=1,OD=1,RTemp=20,ATemp=RTemp,IRWTemp=RTemp,IRT=1,RH=50,
   # example with emissivity=0.95, distance=1m, window transmission=0.96, all temperatures=20C, 
   # 50% relative humidity
   # raw2temp(18109,0.95,1,20,20,20,0.96,50) 
-  # default calibration constants for my FLIR camera will be used if you leave out the calibration data
+  # default calibration constants for my FLIR camera will be used if you leave out the 
+  # calibration data
   
   # raw: A/D bit signal from FLIR file
   # FLIR .seq files and .fcf files store data in a 16-bit encoded value. 
-  # This means it can range from 0 up to 65535.  This is referred to as the raw value.  The raw value is 
-  # actually what the sensor detects which is related to the radiance hitting the sensor
-  # At the factory, each sensor has been calibrated against a blackbody radiation source so calibration
-  # values to conver the raw signal into the expected temperature of a blackbody radiator are provided.
-  # Since the sensors do not pick up all wavelengths of light, the calibration can be estimated using 
-  # a limited version of Planck's law.  But the blackbody calibration is still critical to this.
+  # This means it can range from 0 up to 65535.  This is referred to as the raw value.  
+  # The raw value isactually what the sensor detects which is related to the radiance hitting 
+  # the sensor.
+  # At the factory, each sensor has been calibrated against a blackbody radiation source so 
+  # calibration values to conver the raw signal into the expected temperature of a blackbody 
+  # radiator are provided.
+  # Since the sensors do not pick up all wavelengths of light, the calibration can be estimated
+  # using a limited version of Planck's law.  But the blackbody calibration is still critical
+  # to this.
   
   # E: Emissivity - default 1, should be ~0.95 to 0.97 depending on source
   # OD: Object distance in metres
@@ -30,7 +34,7 @@ function(raw,E=1,OD=1,RTemp=20,ATemp=RTemp,IRWTemp=RTemp,IRT=1,RH=50,
   
   # Note: PR1, PR2, PB, PF, and PO are specific to each camera and result from the calibration at factory
   # of the camera's Raw data signal recording from a blackbody radiation source
-  # Calibration Constants                 (Glenn's FLIR, Ray's T300(25o) Ray's, T300(telephoto), Glenn's Mikron )
+  # Calibration Constants                 (A FLIR SC660, A FLIR T300(25o), T300(telephoto), A Mikron 7515)
   # PR1: PlanckR1 calibration constant from FLIR file  21106.77       14364.633     14906.216       21106.77
   # PB: PlanckB calibration constant from FLIR file    1501           1385.4        1396.5          9758.743281
   # PF: PlanckF calibration constant from FLIR file    1              1             1               29.37648768
