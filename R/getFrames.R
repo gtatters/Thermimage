d39cba52cf26d26cc2d9986baef9853677742584
Glenn Tattersall <gtatters@brocku.ca>
2017-10-04 21:34:23 +0800

    minor edit to getFrames

--- R/getFrames.R
+++ R/getFrames.R
@@ -2,11 +2,14 @@
 #' 
 getFrames<-function(vidfile, framestarts, w=640, h=480, l=w*h, byte.length=2, reverse=FALSE)
   {
+  if(exists("reverse")==FALSE) reverse==FALSE
+  
   to.read <- file(vidfile, "rb") 
   # set to.read file.  rb means read binary
   seek(to.read, where=(framestarts)*byte.length-2, origin="start")
   fram<-readBin(to.read, integer(), n=l, size=byte.length, endian = "little", signed=FALSE)
   close(to.read)
+  
   if(reverse==TRUE){
     return(rev(fram))
   }
