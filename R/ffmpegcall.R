5cf43ea2e4bea580f0139a1f3e2de643ba16ff0c
Glenn Tattersall <gtatters@brocku.ca>
2017-12-21 20:59:30 -0500

    minor fix to ffmpegcall

--- R/ffmpegcall.R
+++ R/ffmpegcall.R
@@ -20,12 +20,12 @@ ffmpegcall<-function(filenameroot, filenamesuffix="%05d", filenameext="jpegls", 
   
   if(outputfiletype=="png"){
     # ffmpeg -f image2 -vcodec jpegls -i frame%05d.jpegls -f image2 -vcodec png frame%05d.png -y
-    args=c("-f", "image2", "-vcodec", incompresstype, "-s", res.in, "-i", shQuote(filename), "-f", "image2", "-vcodec", "png", "-s", res.out, shQuote(filename.png), "-y")
+    args=c("-f", "image2", "-vcodec", incompresstype, "-s", res.in, "-i", shQuote(filename), "-f", "image2", "-vcodec", "png",  "-s", res.out, shQuote(filename.png), "-y")
     system2('ffmpeg', args=args, stdout=TRUE)
   }
   if(outputfiletype=="avi"){
     # ffmpeg -r 30 -f image2 -vcodec jpegls -s 1024x768 -i frame%05d.jpegls -vcodec png -s 1024x768 frame.avi -y
-    args=c("-r", fr, "-f", "image2", "-vcodec", incompresstype, "-s", res.in, "-i", shQuote(filename), "-vcodec", outputcompresstype, "-s", res.out, shQuote(filename.avi), "-y")
+    args=c("-r", fr, "-f", "image2", "-vcodec", incompresstype, "-s", res.in, "-i", shQuote(filename), "-vcodec", outputcompresstype,  "-s", res.out, shQuote(filename.avi), "-y")
     system2('ffmpeg', args=args, stdout=TRUE)
   }
 }
