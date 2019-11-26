efe3c8ef26bd9e51be014e8a9708199268c55bb9
Glenn Tattersall <gtatters@brocku.ca>
2019-11-26 14:32:22 -0500

    edited ffmpegcall to test if OS is windows to properly call the correct ffmpeg file.  Added "exe"

--- R/ffmpegcall.R
+++ R/ffmpegcall.R
@@ -5,6 +5,16 @@ ffmpegcall<-function(filenameroot, filenamesuffix="%05d", filenameext="jpegls", 
  
    # for use on a folder of numbered images, use "filename%05d.jpegls" to perform on all images
   
+  if (Sys.info()["sysname"]=="Darwin" | Sys.info()["sysname"]=="Linux")
+  {
+    ffmpeg<-"ffmpeg"
+  }
+  
+  if (Sys.info()["sysname"]=="Windows")
+  {
+    ffmpeg<-"ffmpeg.exe"
+  }
+  
   if(!dir.exists(outputfolder)) dir.create(outputfolder)
   
   filename<-paste0(filenameroot, filenamesuffix, ".", filenameext)
@@ -22,19 +32,19 @@ ffmpegcall<-function(filenameroot, filenamesuffix="%05d", filenameext="jpegls", 
     # ffmpeg -f image2 -vcodec jpegls -i frame%05d.jpegls -f image2 -vcodec png frame%05d.png -y
     args=c("-f", "image2", "-vcodec", incompresstype, "-s", res.in, "-i", shQuote(filename), "-f", "image2", "-vcodec", "png",  "-s", res.out, shQuote(filename.png), "-y")
     cat("\n")
-    cat(paste(c("ffmpeg", args)))
+    cat(paste(c(ffmpeg, args)))
     cat("\n")
     cat("\n")
-    system2('ffmpeg', args=args, stdout=TRUE)
+    system2(ffmpeg, args=args, stdout=TRUE)
   }
   if(outputfiletype=="avi"){
     # ffmpeg -r 30 -f image2 -vcodec jpegls -s 1024x768 -i frame%05d.jpegls -vcodec png -s 1024x768 frame.avi -y
     args=c("-r", fr, "-f", "image2", "-vcodec", incompresstype, "-s", res.in, "-i", shQuote(filename), "-vcodec", outputcompresstype,  "-s", res.out, shQuote(filename.avi), "-y")
     cat("\n")
-    cat(paste(c("ffmpeg", args)))
+    cat(paste(c(ffmpeg, args)))
     cat("\n")
     cat("\n")
-    system2('ffmpeg', args=args, stdout=TRUE)
+    system2(ffmpeg, args=args, stdout=TRUE)
   }
 }
 
